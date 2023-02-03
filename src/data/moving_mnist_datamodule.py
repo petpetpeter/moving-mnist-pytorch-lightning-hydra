a8f4ec3d375845470ca614b209331bb5f0bc1e17
petpetpeter <chana.pol.peter@gmail.com>
2023-02-03 13:14:21 +0700

    init

---
+++ src/data/moving_mnist_datamodule.py
@@ -0,0 +1,147 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+
+import numpy as np
+import cv2
+
+class MonvingMnistDataModule(LightningDataModule):
+    """Example of LightningDataModule for MNIST dataset.
+
+    A DataModule implements 5 key methods:
+
+        def prepare_data(self):
+            # things to do on 1 GPU/TPU (not on every GPU/TPU in DDP)
+            # download data, pre-process, split, save to disk, etc...
+        def setup(self, stage):
+            # things to do on every process in DDP
+            # load data, set variables, etc...
+        def train_dataloader(self):
+            # return train dataloader
+        def val_dataloader(self):
+            # return validation dataloader
+        def test_dataloader(self):
+            # return test dataloader
+        def teardown(self):
+            # called on every process in DDP
+            # clean up after fit or test
+
+    This allows you to share a full dataset without explaining how to download,
+    split, transform and process the data.
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    @property
+    def num_classes(self):
+        return 10
+
+    def prepare_data(self):
+        """Download data if needed.
+
+        Do not use it to assign state (self.x = y).
+        """
+        #download with wget
+        #
+        #result = subprocess.run(['wget', 'http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy'], stdout=subprocess.PIPE)
+        return 1
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
+        careful not to execute things like random split twice!
+        """
+        data_shape = (10,1,64,64) # (seq_len, Channel, height, width)
+        # load and split datasets only if not loaded already
+        data_numpy = np.load('/home/peter/py_ws/ltsmae/lightning-hydra-template/data/Moving_MNIST/mnist_test_seq.npy')
+        # (seq_len,num_seq,height,width)
+        # swap to (num_seq,seq_len,height,width)
+        data_numpy = np.swapaxes(data_numpy,0,1)
+        #random split to train, val, test 0.7, 0.15, 0.15
+        len_train = int(data_numpy.shape[0]*0.7)
+        len_val = int(data_numpy.shape[0]*0.15)
+        len_test = data_numpy.shape[0] - len_train - len_val
+        self.data_train = data_numpy[:len_train]
+        self.data_val = data_numpy[len_train:len_train+len_val]
+        self.data_test = data_numpy[len_train+len_val:]
+
+        
+            
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None):
+        """Clean up after fit or test."""
+        pass
+
+    def state_dict(self):
+        """Extra things to save to checkpoint."""
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        """Things to do when loading checkpoint."""
+        pass
+
+
+if __name__ == "__main__":
+    mmd = MonvingMnistDataModule()
+    mmd.prepare_data()
+    mmd.setup()
+    first_batch = next(iter(mmd.train_dataloader()))
+    print(first_batch.shape)
+
+
